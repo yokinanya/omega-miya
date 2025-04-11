f662465e8051ec2cccbaf0769624ca82ccd9ec68
Ailitonia <41713304+ailitonia@users.noreply.github.com>
2025-04-11 15:01:43 +0800

    feat(utils): `openai_api` 本地图片处理方法增加格式转换

--- src/utils/openai_api/helpers.py
+++ src/utils/openai_api/helpers.py
@@ -3,14 +3,16 @@
 @Date           : 2025/2/12 15:39:43
 @FileName       : helpers.py
 @Project        : omega-miya
-@Description    : 
+@Description    : 本地文件处理
 @GitHub         : https://github.com/Ailitonia
 @Software       : PyCharm 
 """
 
 import base64
+from io import BytesIO
 from typing import TYPE_CHECKING
 
+from PIL import Image
 from nonebot.utils import run_sync
 
 if TYPE_CHECKING:
@@ -18,22 +20,39 @@ if TYPE_CHECKING:
 
 
 @run_sync
-def base64_encode(content: bytes, *, encoding: str = 'utf-8') -> str:
+def _base64_encode(content: bytes, *, encoding: str = 'utf-8') -> str:
     return base64.b64encode(content).decode(encoding)
+
+
+@run_sync
+def _convert_image_format(input_image: bytes, format_: str = 'webp') -> bytes:
+    with BytesIO(input_image) as input_bf:
+        with Image.open(input_bf) as image:
+            with BytesIO() as output_bf:
+                image.save(output_bf, format=format_)
+                content = output_bf.getvalue()
+    return content
 
 
 async def encode_local_audio(audio: 'BaseResource') -> tuple[str, str]:
     """将本地音频文件编码成 base64 格式的 input_audio, 返回 (data, format) 的数组"""
     async with audio.async_open('rb') as af:
         content = await af.read()
-    return await base64_encode(content), audio.path.suffix.removeprefix('.')
+    return await _base64_encode(content), audio.path.suffix.removeprefix('.')
 
 
-async def encode_local_image(image: 'BaseResource') -> str:
+async def encode_local_image(image: 'BaseResource', *, convert_format: str | None = None) -> str:
     """将本地图片文件编码成 base64 格式的 image_url"""
     async with image.async_open('rb') as af:
         content = await af.read()
-    return f'data:image/{image.path.suffix.removeprefix('.')};base64,{await base64_encode(content)}'
+
+    if convert_format is not None:
+        content = await _convert_image_format(content, format_=convert_format)
+        format_suffix = convert_format
+    else:
+        format_suffix = image.path.suffix.removeprefix('.')
+
+    return f'data:image/{format_suffix};base64,{await _base64_encode(content)}'
 
 
 __all__ = [
